97d57b99b4323bb3c4de25754cdb0a587167bbb5
AndreyElagin2i <82924250+AndreyElagin2i@users.noreply.github.com>
2021-05-26 18:32:18 +0300

    Add files via upload

---
+++ задание 11/задание 11/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace задание_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b = 0;
+            for (a = -15; a <= 15; a++)
+            {
+
+                if (a % 3 == 0)
+                {
+                     b = b + 1;
+                }
+               
+            }
+            b = b - 1;
+            Console.WriteLine("Чисел кратных 3 = " + b);
+            Console.WriteLine("Не считая ноля.");
+
+        }
+    }
+}
