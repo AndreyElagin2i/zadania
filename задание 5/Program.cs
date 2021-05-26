6ccd1adb66ca863c29165d0b3f4bcf89a390b119
AndreyElagin2i <82924250+AndreyElagin2i@users.noreply.github.com>
2021-05-26 15:36:19 +0300

    Add files via upload

---
+++ задание 5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите 3 числа");
+            int a = Int32.Parse(Console.ReadLine());
+            int b = Int32.Parse(Console.ReadLine());
+            int c = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Числа кратные 5");
+            if (a% 5 ==0)
+            {
+                Console.WriteLine(a);
+            }
+            if (b%5==0)
+            {
+                Console.WriteLine(b);
+            }
+            if (c%5==0)
+            {
+                Console.WriteLine(c);
+            }
+            Console.ReadKey();
+        }
+    }
+}
