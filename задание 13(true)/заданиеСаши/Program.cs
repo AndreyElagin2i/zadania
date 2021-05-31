2939edf6243cc485264c19fc4e401f26d0aff636
AndreyElagin2i <82924250+AndreyElagin2i@users.noreply.github.com>
2021-05-31 21:48:52 +0300

    Add files via upload

---
+++ задание 13(true)/заданиеСаши/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace заданиеСаши
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] myArray = new int[n];
+            int i;
+            for (i = 0; i < n; i++)
+                myArray[i] = i * i;
+            for (i = 0; i < n; i++)
+                Console.Write(myArray[i]+" ");
+            Console.WriteLine();
+            for (i = 0; i < n; i++)
+                if (myArray[i] % 8 == 0)
+                {
+                    myArray[i] = 0;
+                }
+            for (i = 0; i < n; i++)
+                Console.Write(myArray[i] + " ");
+        }
+    }
+}
