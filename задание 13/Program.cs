49688e5fea45b07a639e19e6893acfcd35bd56e9
AndreyElagin2i <82924250+AndreyElagin2i@users.noreply.github.com>
2021-05-31 10:53:39 +0300

    Add files via upload

---
+++ задание 13/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace задание_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a,b,x,y,f,z,c;
+            Console.WriteLine("Введите a,b,x,c");
+            a=Convert.ToDouble(Console.ReadLine());
+            b = Convert.ToDouble(Console.ReadLine());
+            x = Convert.ToDouble(Console.ReadLine());
+            c = Convert.ToDouble(Console.ReadLine());
+            y = (Math.Sin(3 *x / 3) - (Math.Cos (4 * x )/ 4));
+            f = (Math.Pow(x, 4) + a * Math.Pow(x, 2) + b * x)/c;
+            z = (5 * y + 3 * f) / x;
+            Console.WriteLine("y=" + y);
+            Console.WriteLine("f=" + f);
+            Console.WriteLine("z=" + z);
+            Console.ReadKey();
+        }
+    }
+}
